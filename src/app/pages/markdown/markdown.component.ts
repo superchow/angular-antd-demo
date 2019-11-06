c19a37610edeb56f55f22a9323a9ab8f5981bc3d
周卫忠 <2665752129@qq.com>
2019-11-06 14:27:51 +0800

    ngx-markdown使用完善

--- src/app/pages/markdown/markdown.component.ts
+++ src/app/pages/markdown/markdown.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { NzCopyToClipboardService, NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-markdown',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./markdown.component.less']
 })
 export class MarkdownComponent implements OnInit {
-  remark: string;
-  constructor() { }
+  remarkHead: string = '';
+  remarkBody: string = '';
+  public get remark(): string {
+    const breakStr = '\t\r\n';
+    const {remarkHead, remarkBody} = this;
+    return `![what' new](https://cdn.gfzq.cn/nbopstatic/email_title_3.png "新一代集中交易运营平台")
+      ${breakStr}${remarkHead}${breakStr} ![Data statistics](https://cdn.gfzq.cn/nbopstatic/email_data_3.png "新一代集中交易运营平台")
+      ${breakStr}${remarkBody}`;
+  }
+
+  constructor(
+    private elementRef: ElementRef,
+    private copyToClipboard: NzCopyToClipboardService,
+    private message: NzMessageService
+  ) {
+    window[`MarkdownComponent`] = this;
+  }
 
   ngOnInit() {
   }
 
+  async copyMarkdownHtml() {
+    const markdownContainer = this.elementRef.nativeElement.querySelector('#markdownContainer');
+    // markdownContainer.select();
+    // document.execCommand('Copy', false, null);
+    const htmlText =
+    `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <title>NBOP-EDM-H5</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+      <body style="margin: 0; padding: 0;">
+      ${markdownContainer.innerHTML}
+      </body>
+    </html>`;
+    const result = await this.copyToClipboard.copy(htmlText);
+    if (result) {
+      this.message.success('复制成功！');
+    } else {
+      this.message.error('复制失败！');
+    }
+  }
 }
