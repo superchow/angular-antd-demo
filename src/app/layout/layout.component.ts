86554032eea579a18c2b457ac842a2efb2338a0b
周卫忠 <2665752129@qq.com>
2019-11-08 15:55:48 +0800

    Angular组件选择器示例写法示例

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -8,8 +8,17 @@ export interface MenuRoute {
 }
 export type MenuRoutes = MenuRoute[];
 
+/**
+ * Angular组件化除了自定义标签，还可以在selector元数据里配置样式、属性的调用方式。selector总共有6种配置方式：
+ * selector: 'element-name'//自定义标签选择器
+ * selector: '.class'//样式选择器
+ * selector: '[attribute]'//属性选择器
+ * selector: '[attribute=value]'//属性值选择器
+ * selector: ':not(sub_selector)'//取反选择器
+ * selector: 'selector1, selector2'//多种选择器
+ */
 @Component({
-  selector: 'layout',
+  selector: 'layout,[layout]',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.less']
 })
