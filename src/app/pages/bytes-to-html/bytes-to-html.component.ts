958551a6629bbcfa637d047237ee9d89ffc49c5a
周卫忠 <2665752129@qq.com>
2019-11-11 14:11:42 +0800

    stylelint实现运行

--- src/app/pages/bytes-to-html/bytes-to-html.component.ts
+++ src/app/pages/bytes-to-html/bytes-to-html.component.ts
@@ -4,26 +4,25 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'bytesToHtml',
   templateUrl: './bytes-to-html.component.html',
-  styleUrls: ['./bytes-to-html.component.less']
+  styleUrls: ['./bytes-to-html.component.less'],
 })
 export class BytesToHtmlComponent implements OnInit {
   bytesHtml: string;
   byteStr: string;
-  constructor(
-    public http: HttpClient
-  ) {
-    // tslint:disable-next-line:no-string-literal
-    window['bytesToHtmlComponent'] = this;
+  constructor(public http: HttpClient) {
+    window[`bytesToHtmlComponent`] = this;
   }
 
   ngOnInit() {
-    this.http.get('/assets/bytes.txt', {
-      responseType: 'text',
-    }).toPromise()
-    .then(txt => {
-      this.byteStr = txt;
-      this.bytesHtml = this.b64DecodeUnicode(txt);
-    });
+    this.http
+      .get('/assets/bytes.txt', {
+        responseType: 'text',
+      })
+      .toPromise()
+      .then(txt => {
+        this.byteStr = txt;
+        this.bytesHtml = this.b64DecodeUnicode(txt);
+      });
     // this.http.get('/assets/bytes.html', {
     //   responseType: 'blob'
     // }).toPromise()
@@ -39,7 +38,7 @@ export class BytesToHtmlComponent implements OnInit {
 
   arrayBufferToString(arrBytes: ArrayBuffer): Promise<string> {
     return new Promise(resolve => {
-      const blob = new Blob([arrBytes], { type: 'text/html'});
+      const blob = new Blob([arrBytes], { type: 'text/html' });
       const reader = new FileReader();
       reader.readAsText(blob, 'utf-8');
       // tslint:disable-next-line:only-arrow-functions
@@ -49,9 +48,14 @@ export class BytesToHtmlComponent implements OnInit {
     });
   }
   b64DecodeUnicode(str: string) {
-    return decodeURIComponent(atob(str).split('').map((c) => {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    return decodeURIComponent(
+      atob(str)
+        .split('')
+        .map(c => {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+    );
   }
   // byteToString(arr: any) {
   //   if (typeof arr === 'string') {
